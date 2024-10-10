86610293f13d7eb2ce7535d7596de2e253955280
idinsmore1 <iandinsmore1@gmail.com>
2024-10-10 16:24:43 -0400

    added some basic constants

---
+++ src/aurora/consts.py
@@ -0,0 +1,8 @@
+import polars as pl
+from pathlib import Path
+
+source_path = Path(__file__).parent.parent
+
+phecode_defs = pl.scan_csv(source_path / 'data/phecode_definitions1.2.csv', schema_overrides={'phecode': pl.String})
+male_specific_codes = phecode_defs.filter(pl.col('sex') == 'Male').collect()['phecode'].to_list()
+female_specific_codes = phecode_defs.filter(pl.col('sex') == "Female").collect()['phecode'].to_list()
--- src/aurora/polars_aurora.py
+++ src/aurora/polars_aurora.py
@@ -1,11 +1,34 @@
 import polars as pl
+import polars.selectors as cs
 import numpy as np
+
+from aurora.consts import male_specific_codes, female_specific_codes
 
 @pl.api.register_dataframe_namespace('phewas')
 @pl.api.register_lazyframe_namespace('phewas')
 class PhewasFrame:
     def __init__(self, df: pl.DataFrame) -> None:
         self._df = df
+
+    def handle_sex_specific_codes(self, drop=False) -> pl.DataFrame:
+        """
+        Handle sex-specific codes in the DataFrame.
+
+        This method processes the DataFrame to handle sex-specific codes based on the specified method.
+
+        Parameters:
+        drop: bool -> Drop the sex specific code names from the analysis. Default = False.
+
+        Returns:
+        pl.DataFrame: The processed DataFrame with sex-specific codes handled.
+        """
+        col_names = self._df.collect_schema().names()
+        male_codes_in_df = [col for col in col_names if col in male_specific_codes]
+        female_codes_in_df = [col for col in col_names if col in female_specific_codes]
+        if drop:
+            return self._df.select(pl.all().exclude(*male_codes_in_df, *female_codes_in_df))
+        return self._df
+        
 
     def handle_missing(self, predictors: list[str], method=None) -> pl.DataFrame:
         """
@@ -33,16 +56,26 @@ class PhewasFrame:
             raise ValueError(f"Unknown method '{method}'. Valid strategies are: {valid_strategies}")
         if method is None:
             new_df = self._df.drop_nulls(subset=predictors)
-            if new_df.height != self._df.height:
-                print(f"Dropped {self._df.height - new_df.height} rows with missing values.")
+            if isinstance(new_df, pl.DataFrame):
+                if new_df.height != self._df.height:
+                    print(f"Dropped {self._df.height - new_df.height} rows with missing values.")
+            else:
+                new_height = new_df.select(pl.len()).collect().item()
+                old_height = self._df.select(pl.len()).collect().item()
+                if new_height != old_height:
+                    print(f"Dropped {old_height - new_height} rows with missing values.")
         else:
             new_df = self._df.fill_null(subset=predictors, strategy=method)
         return new_df
 
     def filter_by_count(self, phenotypes: list[str], min_cases: int=20) -> pl.DataFrame:
         pheno_data = self._df.select(phenotypes)
-        case_counts = pheno_data.sum().collect().transpose()
-        total_counts = pheno_data.count().collect().transpose()
+        if isinstance(pheno_data, pl.LazyFrame):
+            case_counts = pheno_data.sum().collect().transpose()
+            total_counts = pheno_data.count().collect().transpose()
+        else:
+            case_counts = pheno_data.sum().transpose()
+            total_counts = pheno_data.count().transpose()
         control_counts = total_counts - case_counts
         target_df = pl.DataFrame({
             'phenotype': phenotypes,
